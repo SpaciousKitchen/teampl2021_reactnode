0cae0a7bc0ee8a053a6e9c4b2ff3c8dab926dc0b
song <johyesong8686@gmail.com>
2021-02-19 01:33:51 +0900

    feat:Sequelize 및 초기 셋팅

--- server/index.js
+++ server/index.js
@@ -1,41 +1,32 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require('cors')
-
+const cors = require("cors");
+const sequelize = require("./models").sequelize;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+sequelize
+  .sync()
+  .then(() => {
+    console.log("DB 연결중");
+  })
+  .catch(console.error);
 
-const config = require("./config/key");
-
-// const mongoose = require("mongoose");
-// mongoose
-//   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
-//   .catch(err => console.error(err));
-
-const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
-
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/product', require('./routes/product'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/product", require("./routes/product"));
 
-
-//use this to show the image you have in node js server to client (react js)
-//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
   // Set static folder
   app.use(express.static("client/build"));
 
@@ -45,8 +36,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  console.log(`Server Running at ${port}`);
 });
--- server/models/User.js
+++ server/models/User.js
@@ -1,98 +1,36 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-const moment = require('moment');
-
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "user",
+    {
+      userId: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      provider: {
+        type: DataTypes.STRING(30),
+        allowNull: true,
+      },
     },
-    email: {
-        type: String,
-        trim: true,
-        unique: 1
-    },
-    password: {
-        type: String,
-        minglength: 5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    cart: {
-        type: Array,
-        default: []
-    },
-    history: {
-        type: Array,
-        default: []
-    },
-    image: String,
-    token: {
-        type: String,
-    },
-    tokenExp: {
-        type: Number
+    {
+      modelName: "User",
+      tableName: "users",
+      charset: "utf8",
+      collate: "utf8_general_ci",
+      sequelize,
     }
-})
-
-userSchema.pre('save', function (next) {
-    var user = this;
-
-    if (user.isModified('password')) {
-        console.log('password changed')
-        bcrypt.genSalt(saltRounds, function (err, salt) {
-            if (err) return next(err);
-
-            bcrypt.hash(user.password, salt, function (err, hash) {
-                if (err) return next(err);
-                user.password = hash
-                next()
-            })
-        })
-    } else {
-        next()
-    }
-});
-
-userSchema.methods.comparePassword = function (plainPassword, cb) {
-    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch)
-    })
-}
-
-userSchema.methods.generateToken = function (cb) {
-    var user = this;
-    var token = jwt.sign(user._id.toHexString(), 'secret')
-    var oneHour = moment().add(1, 'hour').valueOf();
-
-    user.tokenExp = oneHour;
-    user.token = token;
-    user.save(function (err, user) {
-        if (err) return cb(err)
-        cb(null, user);
-    })
-}
-
-userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
-
-    jwt.verify(token, 'secret', function (err, decode) {
-        user.findOne({ "_id": decode, "token": token }, function (err, user) {
-            if (err) return cb(err);
-            cb(null, user);
-        })
-    })
-}
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = { User }
+  );
+};
