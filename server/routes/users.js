43a541300fe9e8eeb210e3c22aaa2401182a7bb3
song <johyesong8686@gmail.com>
2021-02-22 01:47:19 +0900

    feat(back): 인증 메일 템플렛 적용

--- server/routes/users.js
+++ server/routes/users.js
@@ -50,11 +50,23 @@ router.post("/register", async (req, res, next) => {
     let message = {
       from: `React Shop <${process.env.MAIL_USER}>`, // sender address
       to: req.body.email, // list of receivers
-      subject: "안녕하세요. 이메일 인증을 완료해주세요", // Subject line
-      html: `<p>안녕하세요.
-    React Shop 에서 가입하신 이메일 계정 확인을 위한 인증 메일입니다.
-    아래 버튼을 클릭하여 계정 인증을 완료해 주세요.</p>
-    <a href=${authUrl}>인증 링크 바로가기</a>`, // html body
+      subject: `[React Shop] ${req.body.name} 님, 이메일 인증을 완료해주세요`, // Subject line
+      html: `<div style="font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 540px; height: 600px; border-top: 3px solid #1890ff; margin: 100px auto; padding: 30px 0; box-sizing: border-box;">
+    
+      <h1 style="margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;">
+      <span style="color: #1890ff;">메일인증</span> 안내입니다.
+    </h1>
+    <p style="font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;">
+      안녕하세요.<strong>${req.body.name}</strong> 님<br />
+      REACT_SHOP 에 가입해 주셔서 진심으로 감사드립니다.<br />
+      아래 링크를 클릭하여 회원가입을 완료해 주세요.<br />
+      감사합니다.
+    </p>
+    <a href=${authUrl}>인증 링크 바로가기</a>
+    <div style="font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 540px; height: 600px; border-top: 2px solid #d9d9d9; margin: 100px auto; padding: 30px 0; box-sizing: border-box;">
+
+    
+  </div>`,
     };
 
     transporter.sendMail(message, (err, info) => {
